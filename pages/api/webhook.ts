4f18797dbfcd4338606082c919beb61ff6e0538d
Daniel Büchele <daniel@buechele.cc>
2021-02-05 21:59:22 +0000

    fix?

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -18,6 +18,25 @@ VOD
 
 */
 
+type Payload = {
+  object: "page";
+  entry: Entry[];
+};
+
+type Entry = {
+  id: string;
+  time: number;
+  changes: Change[];
+};
+
+type Change = {
+  value: {
+    id: string;
+    status: VideoStatus;
+  };
+  field: "live_videos";
+};
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const mode = req.query["hub.mode"];
   const challenge = req.query["hub.challenge"];
@@ -26,81 +45,65 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return res.send(challenge);
   }
 
-  const body = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
+  const body: Payload =
+    typeof req.body === "string" ? JSON.parse(req.body) : req.body;
   console.log(JSON.stringify(body));
 
-  const handleEvents = body?.entry?.flatMap(
-    (entry: { id: string; time: number; changes: any[] }) =>
-      entry.changes.map(
-        async (change: {
-          value: {
-            id: string;
-            status: VideoStatus;
-          };
-          field: string;
-        }) => {
-          if (
-            change.field !== "live_videos" ||
-            change.value.status === "unpublished"
-          ) {
-            return;
-          }
+  const handleEvents = body?.entry?.flatMap((entry) =>
+    entry.changes.map(async (change) => {
+      if (change.field !== "live_videos") {
+        return;
+      }
 
-          const existing = await prisma.live.findUnique({
-            where: {
-              broadcastId: change.value.id,
-            },
-          });
+      const existing = await prisma.live.findUnique({
+        where: {
+          broadcastId: change.value.id,
+        },
+      });
 
-          const fb: {
-            video: {
-              id: string;
-            };
-            broadcast_start_time?: string; //"2021-02-01T23:54:16+0000",
-            planned_start_time?: string; //"2021-02-01T23:54:16+0000",
-            title: string;
-            description: string;
-            id: string;
-          } = await fetch(
-            `https://graph.facebook.com/${change.value.id}?access_token=${process.env.FB_TOKEN}&fields=video,planned_start_time,broadcast_start_time,title,description`
-          ).then((res) => res.json());
+      const fb: {
+        video: {
+          id: string;
+        };
+        broadcast_start_time?: string; //"2021-02-01T23:54:16+0000",
+        planned_start_time?: string; //"2021-02-01T23:54:16+0000",
+        title: string;
+        description: string;
+        id: string;
+      } = await fetch(
+        `https://graph.facebook.com/${change.value.id}?access_token=${process.env.FB_TOKEN}&fields=video,planned_start_time,broadcast_start_time,title,description`
+      ).then((res) => res.json());
 
-          console.log(JSON.stringify(fb));
+      if (change.value.status === "live" && existing?.status !== "live") {
+        await sendSMS(fb.title);
+      }
 
-          if (change.value.status === "live" && existing?.status !== "live") {
-            await sendSMS(fb.title);
-          }
+      const payload: Omit<Prisma.liveUncheckedCreateInput, "broadcastId"> = {
+        status: change.value.status,
+        updatedAt: new Date(),
+        pageId: entry.id,
+        videoId: fb.video.id,
+        title: fb.title,
+        description: fb.description,
+        broadcast_start_time: fb.broadcast_start_time
+          ? new Date(fb.broadcast_start_time)
+          : null,
+        planned_start_time: fb.planned_start_time
+          ? new Date(fb.planned_start_time)
+          : null,
+      };
 
-          const payload: Omit<
-            Prisma.liveUncheckedCreateInput,
-            "broadcastId"
-          > = {
-            status: change.value.status,
-            updatedAt: new Date(),
-            pageId: entry.id,
-            videoId: fb.video.id,
-            title: fb.title,
-            description: fb.description,
-            broadcast_start_time: fb.broadcast_start_time
-              ? new Date(fb.broadcast_start_time)
-              : null,
-            planned_start_time: fb.planned_start_time
-              ? new Date(fb.planned_start_time)
-              : null,
-          };
-
-          await prisma.live.upsert({
-            create: {
-              ...payload,
-              broadcastId: change.value.id,
-            },
-            update: payload,
-            where: {
-              broadcastId: change.value.id,
-            },
-          });
-        }
-      )
+      await prisma.live.upsert({
+        create: {
+          ...payload,
+          broadcastId: change.value.id,
+        },
+        update: payload,
+        where: {
+          broadcastId: change.value.id,
+        },
+      });
+    })
   );
 
   await Promise.all(handleEvents ?? []);
