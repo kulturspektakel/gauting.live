34c3897225b4c11e57765bafa370bfde632852b3
Daniel Büchele <daniel@buechele.cc>
2021-02-26 20:21:02 +0000

    Revert "Revert "vide""
    This reverts commit 6f597911f49c34779f99c1406440589e988925be.
    
    # Conflicts:
    #	components/Video.tsx

--- components/Video.tsx
+++ components/Video.tsx
@@ -8,17 +8,7 @@ export default function Video({
   return (
     <div className="video">
       <div className="videoInner">
-        <iframe
-          width="100%"
-          height="100%"
-          frameBorder="0"
-          allowFullScreen
-          scrolling="no"
-          allow="encrypted-media"
-          src={`https://www.youtube.com/embed/${id}?autoplay=${
-            autoPlay ? "1" : "0"
-          }&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&vq=hd1080`}
-        />
+        <a href={`https://www.youtube.com/watch?v=${id}`}>▶︎ Play</a>
       </div>
     </div>
   );
